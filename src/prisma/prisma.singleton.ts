ad4bfb6ef65b65cbb4675c82bd831858aca29b21
Wellington Costa <wellingtonvell@gmail.com>
2024-06-16 14:11:34 -0300

    feat(prisma): Create a utility file to manage a single instance of `PrismaService`

---
+++ src/prisma/prisma.singleton.ts
@@ -0,0 +1,10 @@
+import { PrismaService } from './prisma.service';
+
+let prisma: PrismaService;
+
+export const getPrismaService = (): PrismaService => {
+  if (!prisma) {
+    prisma = new PrismaService();
+  }
+  return prisma;
+};
