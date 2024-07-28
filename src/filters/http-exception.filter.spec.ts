68c327c7873ab1fcd0a2ab58c1cad341230b0616
WellingtonVell <wellingtonvell@gmail.com>
2024-07-28 18:02:02 -0300

    fix(http-exceptions): correct test for HTTP exceptions filter

--- src/filters/http-exception.filter.spec.ts
+++ src/filters/http-exception.filter.spec.ts
@@ -1,29 +1,190 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpExceptionFilter } from './http-exception.filter';
 
-@Controller('test')
-export class TestController {
-  @Get('bad-request')
-  badRequest() {
-    throw new HttpException('Bad Request Example', HttpStatus.BAD_REQUEST);
-  }
+describe('HttpExceptionFilter', () => {
+  let exceptionFilter: HttpExceptionFilter;
 
-  @Get('unauthorized')
-  unauthorized() {
-    throw new HttpException('Unauthorized Example', HttpStatus.UNAUTHORIZED);
-  }
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        HttpExceptionFilter,
+        {
+          provide: APP_FILTER,
+          useClass: HttpExceptionFilter,
+        },
+      ],
+    }).compile();
 
-  @Get('forbidden')
-  forbidden() {
-    throw new HttpException('Forbidden Example', HttpStatus.FORBIDDEN);
-  }
+    exceptionFilter = module.get<HttpExceptionFilter>(HttpExceptionFilter);
+  });
 
-  @Get('not-found')
-  notFound() {
-    throw new HttpException('Not Found Example', HttpStatus.NOT_FOUND);
-  }
+  it('should return default message for Success', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
 
-  @Get('server-error')
-  serverError() {
-    throw new HttpException('Internal Server Error Example', HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-}
+    const exception = new HttpException('', HttpStatus.OK);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.OK);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.OK,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Solicitação realizada com sucesso',
+    });
+  });
+
+  it('should return default message for Created', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.CREATED);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.CREATED);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.CREATED,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Criado com sucesso',
+    });
+  });
+
+  it('should return default message for ACCEPTED', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.ACCEPTED);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.ACCEPTED);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.ACCEPTED,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Requisição aceita',
+    });
+  });
+
+  it('should return default message for BadRequestException', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.BAD_REQUEST);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.BAD_REQUEST);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.BAD_REQUEST,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Requisição inválida',
+    });
+  });
+
+  it('should return default message for UnauthorizedException', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.UNAUTHORIZED);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.UNAUTHORIZED);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.UNAUTHORIZED,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Não autorizado',
+    });
+  });
+
+  it('should return default message for ForbiddenException', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.FORBIDDEN);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.FORBIDDEN);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.FORBIDDEN,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Proibido',
+    });
+  });
+
+  it('should return default message for NotFoundException', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.NOT_FOUND);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.NOT_FOUND);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.NOT_FOUND,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Não encontrado',
+    });
+  });
+
+  it('should return default message for InternalServerErrorException', () => {
+    const mockArgumentsHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockImplementation((result) => result),
+      getRequest: jest.fn().mockReturnValue({ url: '/test-url' }),
+    };
+
+    const exception = new HttpException('', HttpStatus.INTERNAL_SERVER_ERROR);
+    exceptionFilter.catch(exception, mockArgumentsHost as any);
+
+    expect(mockArgumentsHost.status).toHaveBeenCalledWith(HttpStatus.INTERNAL_SERVER_ERROR);
+    expect(mockArgumentsHost.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      timestamp: expect.any(String),
+      path: '/test-url',
+      message: 'Erro interno do servidor',
+    });
+  });
+});
